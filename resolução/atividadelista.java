c5e19edfda2225085f9772e88c8ccff12e77aa4c
euzinhakkk <128495520+euzinhakkk@users.noreply.github.com>
2023-05-15 08:12:49 -0300

    Add files via upload

---
+++ resolução/atividadelista.java
@@ -0,0 +1,37 @@
+package Resolução;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class atividadelista {
+
+	public static void main(String[] args) {
+		//List<Integer> lista = new ArrayList<>();
+		
+		//lista.add(10);
+		//lista.add(20);
+		//lista.add(30);
+	
+		//System.out.println(lista);
+		
+		//lista.remove(0);
+		//lista.indexOf(20);
+		
+		//System.out.println(lista);
+		//System.out.println(lista.indexOf(20));
+		
+		List<Integer> numeros = new ArrayList<>();
+		
+		
+		
+		for(int i = 0; i<= 101; i++) {
+			numeros.add(i);
+			
+		}
+		for(int i = 0; i<= 101; i++) {
+			if(i % 2 == 1);
+				System.out.println(numeros.get(i));
+	}
+
+}
+}
---
+++ resolução/aula001.java
@@ -0,0 +1,28 @@
+package Resolução;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class aula001 {
+
+	public static void main(String[] args) {
+		List<Integer> lista = new ArrayList<>();
+		
+		lista.add(10);
+		lista.add(20);
+		lista.add(30);
+	
+		System.out.println(lista);
+		
+		lista.remove(0);
+		lista.indexOf(20);
+		
+		System.out.println(lista);
+		System.out.println(lista.indexOf(20));
+		
+
+	
+
+		
+	}
+}
